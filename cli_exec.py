34065a2a414a1874aadc5a3612640c19bba14b3f
William Taylor <me@willtaylor.info>
2024-07-28 17:58:20 +0100

    Text:true

--- cli_exec.py
+++ cli_exec.py
@@ -2,8 +2,8 @@ import subprocess
 
 
 def execute_pct_list():
-    return subprocess.run(['pct', 'list'])
+    return subprocess.run(['pct', 'list'], text=True)
 
 
 def execute_get_pct_config(pct_id):
-    return subprocess.run(['pct', 'config', f'{pct_id}'])
+    return subprocess.run(['pct', 'config', f'{pct_id}'], text=True)
