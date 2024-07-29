45f8177b833f33cc052b50f3e38cea61a7fd3713
William Taylor <me@willtaylor.info>
2024-07-29 19:44:50 +0100

    capture output

--- cli_exec.py
+++ cli_exec.py
@@ -2,15 +2,15 @@ import subprocess
 
 
 def execute_pct_list():
-    return subprocess.run(['pct', 'list'], text=True)
+    return subprocess.run(['pct', 'list'], text=True, capture_output=True)
 
 
 def execute_get_pct_config(pct_id):
-    return subprocess.run(['pct', 'config', f'{pct_id}'], text=True)
+    return subprocess.run(['pct', 'config', f'{pct_id}'], text=True, capture_output=True)
 
 
 def execute_pct_snapshot_create(pct_id, run_id):
-    return subprocess.run(['pct', 'snapshot', f'{pct_id}'], text=True)
+    return subprocess.run(['pct', 'snapshot', f'{pct_id}'], text=True, capture_output=True)
 
 
 def execute_pct_snapshot_destroy(pct_id, run_id):
