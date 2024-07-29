7e2ecf97f733ff27de05c22ac63587b1e808bf7c
William Taylor <me@willtaylor.info>
2024-07-29 20:41:16 +0100

    Add argparse

--- snapback.py
+++ snapback.py
@@ -3,6 +3,7 @@
 import typing
 import urllib.parse
 import cli_exec
+import argparse
 
 
 def get_all_pct_ids(pct_list_output):
@@ -35,8 +36,46 @@ def get_pct_snapback_config(ct_id, ct_config: typing.Dict[str, str]):
     }
 
 
-if __name__ == "__main__":
+def get_all_snapback_configs():
     all_ct_ids = get_all_pct_ids(cli_exec.execute_pct_list().stdout)
     all_ct_configs = [(ct_id, parse_pct_config(cli_exec.execute_get_pct_config(ct_id).stdout)) for ct_id in all_ct_ids]
-    ct_targets = [get_pct_snapback_config(ct_config[0], ct_config[1]) for ct_config in all_ct_configs if is_ct_snapback_enabled(ct_config[1])]
-    print(ct_targets)
+    return [get_pct_snapback_config(ct_config[0], ct_config[1]) for ct_config in all_ct_configs if is_ct_snapback_enabled(ct_config[1])]
+
+
+def snapback_create(args):
+    snapback_configs = get_all_snapback_configs()
+    if args.verbose:
+        print("Detected snapback configurations:")
+        print(snapback_configs)
+
+
+def snapback_destroy(args):
+    snapback_configs = get_all_snapback_configs()
+    if args.verbose:
+        print("Detected snapback configurations:")
+        print(snapback_configs)
+
+
+def snapback_mount(args):
+    snapback_configs = get_all_snapback_configs()
+    if args.verbose:
+        print("Detected snapback configurations:")
+        print(snapback_configs)
+
+
+if __name__ == "__main__":
+    root_parser = argparse.ArgumentParser("snapback")
+    sub_parsers = root_parser.add_subparsers(help="command help", required=True)
+    root_parser.add_argument("--id", help="Unique id to identify the snapshot", required=True)
+    root_parser.add_argument("--verbose", "-v", help="Verbose output", action="store_true")
+
+    create_parser = sub_parsers.add_parser("create", help="Snapshot all Proxmox CTs with a valid snapback config")
+    create_parser.set_defaults(func=snapback_create)
+
+    mount_parser = sub_parsers.add_parser("mount", help="Mount all snapshots in a common root directory")
+    mount_parser.add_argument("--mount_root", help="Parent directory to mount the snapshots in", required=True)
+
+    destroy_parser = sub_parsers.add_parser("destroy", help="Destroy snapshots with the specified id for all CTs with a valid snapback config")
+
+    args = root_parser.parse_args()
+    args.func(args)
