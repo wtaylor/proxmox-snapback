aa669847bd68256404a119b3abe4d2d89448bdd8
William Taylor <me@willtaylor.info>
2024-08-10 13:21:24 +0100

    Add mount function body and switch logging

--- cli_exec.py
+++ cli_exec.py
@@ -32,6 +32,7 @@ def _raw_execute_pct_snapshot_create(pct_id, snap_name):
 def snapshot_ct(pct_id, snap_name):
     return _raw_execute_pct_snapshot_create(pct_id, snap_name)
 
+
 def _raw_execute_pct_snapshot_destroy(pct_id, snap_name):
     return subprocess.run(['pct', 'delsnapshot', f'{pct_id}', f'{snap_name}', '--force'], text=True, capture_output=True)
 
@@ -39,10 +40,20 @@ def _raw_execute_pct_snapshot_destroy(pct_id, snap_name):
 def delete_snapshot(pct_id, snap_name):
     return _raw_execute_pct_snapshot_destroy(pct_id, snap_name)
 
-def _raw_execute_snapshot_mount(zfs_dataset_snapshot_uid, mount_root):
-    return subprocess.run(['mount', '-t', 'zfs', f'{zfs_dataset_snapshot_uid}', f'{mount_root}'], text=True, capture_output=True)
+
+def _raw_execute_snapshot_mount(zfs_dataset_snapshot_uid, mountpoint):
+    return subprocess.run(['mount', '-t', 'zfs', f'{zfs_dataset_snapshot_uid}', f'{mountpoint}'], text=True, capture_output=True)
 
 
 def _raw_execute_pvesm_path(volume_name):
     return subprocess.run(['pvesm', 'path', f'{volume_name}'], text=True, capture_output=True)
+
+
+def mount_zfs_dataset_snapshot(zfs_dataset_snapshot_uid, mountpoint):
+    return _raw_execute_snapshot_mount(zfs_dataset_snapshot_uid, mountpoint)
+
+
+def get_zfs_dataset_uid_of_ct_volume(volume_name):
+    hostpath = _raw_execute_pvesm_path(volume_name).stdout.strip()
+    return hostpath.split('/', 1)[1]
 # mount -t zfs rpool/data/subvol-203-disk-1@backup-2024-07-16 /mnt/backup/volume-snapshots/203-disk-1
--- snapback.py
+++ snapback.py
@@ -4,9 +4,12 @@ import typing
 import urllib.parse
 import cli_exec
 import argparse
+from pathlib import Path
+from os import listdir
+import logging
 
 
-def is_ct_snapback_enabled(ct_config: typing.Dict[str, str]):
+def is_ct_snapback_enabled(ct_config: typing.Dict[str, str]) -> bool:
     return 'description' in ct_config and "lxc-zfs-snapback" in ct_config['description']
 
 
@@ -28,37 +31,57 @@ def get_all_snapback_configs():
     return [get_pct_snapback_config(ct_config[0], ct_config[1]) for ct_config in all_ct_configs if is_ct_snapback_enabled(ct_config[1])]
 
 
-def snapback_create(args):
+def snapback_create(args, logger: logging.Logger) -> int:
     snapback_configs = get_all_snapback_configs()
-    if args.verbose:
-        print("Detected snapback configurations:")
-        print(snapback_configs)
+    logger.debug("Detected snapback configurations:")
+    logger.debug(snapback_configs)
 
     for snapback_config in snapback_configs:
-        print(f"Snapshotting CT: {snapback_config['id']} - {snapback_config['hostname']}...")
+        logger.info(f"Snapshotting CT: {snapback_config['id']} - {snapback_config['hostname']}...")
         cli_exec.snapshot_ct(snapback_config['id'], args.id)
 
-    print(f"All CTs snapped successfully.")
+    logger.info(f"All CTs snapped successfully.")
+    return 0
 
 
-def snapback_destroy(args):
+def snapback_destroy(args, logger: logging.Logger) -> int:
     snapback_configs = get_all_snapback_configs()
-    if args.verbose:
-        print("Detected snapback configurations:")
-        print(snapback_configs)
+    logger.debug("Detected snapback configurations:")
+    logger.debug(snapback_configs)
 
     for snapback_config in snapback_configs:
-        print(f"Deleting snapshot \"{args.id}\" on CT: {snapback_config['id']} - {snapback_config['hostname']}...")
+        logger.info(f"Deleting snapshot \"{args.id}\" on CT: {snapback_config['id']} - {snapback_config['hostname']}...")
         cli_exec.delete_snapshot(snapback_config['id'], args.id)
 
-    print(f"All CTs snapped successfully.")
+    logger.info(f"Snapshot {args.id} deleted from all CTs successfully.")
+    return 0
 
 
-def snapback_mount(args):
+def snapback_mount(args, logger: logging.Logger) -> int:
     snapback_configs = get_all_snapback_configs()
-    if args.verbose:
-        print("Detected snapback configurations:")
-        print(snapback_configs)
+    logger.debug("Detected snapback configurations:")
+    logger.debug(snapback_configs)
+
+    Path(args.mountpoint).mkdir(parents=True, exist_ok=True)
+    if len(listdir(args.mountpoint)) > 0:
+        logger.error(f"Root mount point: {args.mountpoint} is not empty, aborting.")
+        return 1
+
+    for snapback_config in snapback_configs:
+        for volume in snapback_config['target_volumes']:
+            zfs_snapshot_uid = cli_exec.get_zfs_dataset_uid_of_ct_volume(volume)
+            logger.debug(f"CT Volume: {volume} translated to ZFS Dataset: {zfs_snapshot_uid}")
+
+            volume_mount_point = f"{args.mountpoint}/{volume.replace(':', '--')}"
+            logger.info(f"Mounting {volume} to {volume_mount_point}...")
+            Path(volume_mount_point).mkdir(parents=False, exist_ok=False)
+            mount_result = cli_exec.mount_zfs_dataset_snapshot(zfs_snapshot_uid, volume_mount_point)
+            if mount_result.returncode != 0:
+                logger.error(f"Failed to mount {volume}")
+                logger.error(mount_result.stderr)
+                return 1
+
+    return 0
 
 
 if __name__ == "__main__":
@@ -71,10 +94,14 @@ if __name__ == "__main__":
     create_parser.set_defaults(func=snapback_create)
 
     mount_parser = sub_parsers.add_parser("mount", help="Mount all snapshots in a common root directory")
-    mount_parser.add_argument("--mount_root", help="Parent directory to mount the snapshots in", required=True)
+    mount_parser.add_argument("--mountpoint", help="Parent directory to mount the snapshots in", required=True)
 
     destroy_parser = sub_parsers.add_parser("destroy", help="Destroy snapshots with the specified id for all CTs with a valid snapback config")
     destroy_parser.set_defaults(func=snapback_destroy)
 
     args = root_parser.parse_args()
-    args.func(args)
+    logLevel = logging.DEBUG if args.verbose else logging.INFO
+    logging.basicConfig(level=logLevel)
+    logger = logging.getLogger()
+
+    exit(args.func(args, logger))
